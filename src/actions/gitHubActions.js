4ae00811f86ec626a11c3a0bbd8f5bf277a75c3d
Jerud-Moyer <jerudmoyer@gmail.com>
2020-11-03 14:03:54 -0800

    initial commit

---
+++ src/actions/gitHubActions.js
@@ -0,0 +1,5 @@
+export const SET_USERNAME = 'SET_USERNAME';
+export const setUsername = userName => ({
+  type: SET_USERNAME,
+  payload: userName
+})
---
+++ src/reducers/gitHubReducer.test.js
@@ -0,0 +1,5 @@
+const { getUsername } = require('../actions/gitHubActions');
+
+describe('gitHub reducer', () => {
+  it('handles the SET_USERNAME action')
+})
